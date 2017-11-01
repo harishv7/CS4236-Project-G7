13084dd9aab06f672e0d5a6d75f9abbf58162140
Harish V <harishv207@gmail.com>
2017-11-01 18:12:46 +0800

    Added paths

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
-var MongoClient = require('server');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+    res.render('index', { title: 'Express' });
+});
+
+router.post('/activate', function(req, res, next) {
+    res.render('index', { title: 'Express' });
+});
+
+router.post('/join-game', function(req, res, next) {
+    res.render('index', { title: 'Express' });
+});
+
+router.post('/game-register', function(req, res, next) {
+    res.render('index', { title: 'Express' });
+});
+
+router.post('/reveal-secret', function(req, res, next) {
     res.render('index', { title: 'Express' });
 });
 
