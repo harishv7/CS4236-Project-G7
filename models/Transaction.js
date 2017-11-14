bc2c70ebd834e5619ec1f88c5fc2dc1e6c9e2a11
Teddy Hartanto <hartantoteddy@yahoo.com>
2017-11-14 16:20:39 +0800

    Added a Transaction schema

---
+++ models/Transaction.js
@@ -0,0 +1,42 @@
+var mongoose = require('mongoose');
+var autoIncrement = require('mongoose-auto-increment');
+
+autoIncrement.initialize(mongoose.connection);
+
+var Schema = mongoose.Schema;
+
+var TransactionSchema = new Schema({
+    transaction_type: {
+        type: String,
+        required: true
+    },
+    sender_id: {
+        type: Number,
+        required: true,
+        min: 1
+    },
+    min_bid_value: {
+        type: Number,
+        min: 1
+    },
+    game_id: {
+        type: Number,
+        min: 1
+    },
+    secret_commit: String,
+    guess_commit: String,
+    bid_value: {
+        type: Number,
+        min: 1
+    },
+    secret: Number,
+    r1: String,
+    guess: Number,
+    r2: String
+});
+
+TransactionSchema.plugin(autoIncrement.plugin, { model: 'Transaction', field: 'id', startAt: 1 });
+
+var Transaction = mongoose.model('Transaction', TransactionSchema);
+
+module.exports = Transaction;
