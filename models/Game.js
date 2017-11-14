c8797eafb7ce8bc375d6ee37c0c4ab6374efb377
Kowshik Sundararajan <ksundararajan@authentic8.com>
2017-11-14 16:22:03 +0800

    Updated schema and controllers

--- models/Game.js
+++ models/Game.js
@@ -6,6 +6,11 @@ autoIncrement.initialize(mongoose.connection);
 var Schema = mongoose.Schema;
 
 var GameSchema = new Schema({
+    id: {
+        type: Number,
+        unique: true,
+        dropDups: true
+    },
     min_bid: {
         type: Number,
         required: true,
@@ -15,11 +20,40 @@ var GameSchema = new Schema({
         type: Date,
         required: true
     },
+    state: {
+        type: Number,
+        required: true,
+        default: 0
+    },
     completed: {
         type: Boolean,
         required: true,
         default: false
-    }
+    },
+    num_of_players: {
+        type: Number,
+        required: true,
+        default: 0,
+        min: 0
+    },
+    players: [Number],
+    game_register: [{
+        player_id: {
+            type: Number,
+        },
+        secret: {
+            type: Number
+        },
+        guess: {
+            type: Number
+        },
+        r_one: {
+            type: Number
+        },
+        r_two: {
+            type: Number
+        }
+    }]
 });
 
 GameSchema.plugin(autoIncrement.plugin, { model: 'Game', field: 'id', startAt: 1 });
